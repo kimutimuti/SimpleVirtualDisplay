9c382fb85cd1369aede5a69b0e630d3f152f5f8e
kimutimuti <kimutimuti@gmail.com>
2026-08-18 18:48:02 +0900

    Update AppsUtils.kt

--- app/src/main/java/com/kangrio/virtualdisplay/utils/AppsUtils.kt
+++ app/src/main/java/com/kangrio/virtualdisplay/utils/AppsUtils.kt
@@ -1,84 +1,24 @@
 package com.kangrio.virtualdisplay.utils
 
-import android.app.ActivityOptions
-import android.app.IActivityManager
-import android.app.Presentation
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.pm.IPackageManager
-import android.os.Build
 import android.util.Log
-import android.view.Display
 import com.kangrio.virtualdisplay.helper.ShizukuHelper
-import com.kangrio.virtualdisplay.server.helper.FakeContext
-import rikka.shizuku.ShizukuBinderWrapper
-import rikka.shizuku.SystemServiceHelper
 
 class AppsUtils {
     val TAG = "AppsUtils"
     val shizukuHelper: ShizukuHelper = ShizukuHelper()
 
-    val iPackageManager: IPackageManager =
-        IPackageManager.Stub.asInterface(ShizukuBinderWrapper(SystemServiceHelper.getSystemService("package")))
-
-    val iActivityManager: IActivityManager = IActivityManager.Stub.asInterface(
-        ShizukuBinderWrapper(
-            SystemServiceHelper.getSystemService("activity")
-        )
-    )
-
     fun launchAppTargetDisplay(packageName: String, componentClassName: String, displayId: Int) {
         Log.d(TAG, "launchAppTargetDisplay: $displayId")
 
-
-        val i = Intent(packageName)
-        i.addFlags(Intent.FLAG_ACTIVITY_RETAIN_IN_RECENTS)
-        i.component = ComponentName(packageName, componentClassName)
-
-        val activityOptions: ActivityOptions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            ActivityOptions.makeBasic().setLaunchDisplayId(displayId)
-        } else {
-            ActivityOptions.makeBasic()
-        }
-
-        activityOptions.launchWindowingMode = 1
-
-        iActivityManager.startActivity(
-            null,
-            FakeContext.PACKAGE_NAME,
-            i,
-            null,
-            null,
-            null,
-            0,
-            Intent.FLAG_ACTIVITY_NEW_TASK,
-            null,
-            activityOptions.toBundle()
-        )
-
-
-//        val prsentations =
-//            MyPresentation(App.applicationContext(), displayManager.getDisplay(displayId))
-//
-//
-//        prsentations.show()
-
-//        var tasks = iActivityTaskManager.moveRootTaskToDisplay(10383, displayId)
-//
-//        Log.d(TAG, "launchAppTargetDisplay: ${tasks}")
-
-        /*
-        val cmd = "am start -n $packageName/$componentClassName --display $displayId"
+        // 隠しAPIの代わりに、Shizukuのシェルコマンド(am start)を使って指定ディスプレイでアプリを起動します
+        // --windowingMode 1 をつけることで activityOptions.launchWindowingMode = 1 と同じ効果になります
+        val cmd = "am start -n $packageName/$componentClassName --display $displayId --windowingMode 1"
         Log.d(TAG, "launchAppTargetDisplay: $cmd")
         try {
             shizukuHelper.execInternal(cmd)
         } catch (e: Throwable) {
             e.printStackTrace()
         }
-
-         */
-
     }
 
     fun killApp(packageName: String) {
@@ -90,9 +30,4 @@ class AppsUtils {
             e.printStackTrace()
         }
     }
-}
-
-private class MyPresentation(context: Context?, display: Display?) :
-    Presentation(context, display) {
-
 }
