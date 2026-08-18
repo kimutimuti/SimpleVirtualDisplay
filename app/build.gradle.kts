0015134170c08903d349e71be4fdc154c51d0d73
kimutimuti <kimutimuti@gmail.com>
2026-08-18 17:52:14 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,8 @@ android {
 dependencies {
 
 // Provides stubs for internal system APIs during compilation
-    compileOnly("dev.rikka.hidden.stub:android:34.0.0")
+    compileOnly("dev.rikka.hidden:stub:4.4.0")
+
 
 //    val shizuku_version = "13.1.5"
     val shizuku_version = "13.1.0"
